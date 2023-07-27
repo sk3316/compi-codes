8916e7e9400ee2328c767d19f0a05956a00ca601
sk3316 <sk3316@srmist.edu.in>
2023-07-27 19:05:48 +0530

    back to coding

---
+++ NPTEL/week1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+int main()
+{   
+    // q1.
+    // int n = 4;
+
+    // // std::cout<<std::pow(n,2)<<" "<<std::sqrt(n);
+
+    // cout<<pow(n,2)<<endl;
+    // cout<<sqrt(n)<<endl;
+
+    vector<int> arr;
+    arr.resize(10);
+    for (int i = 0; i < 10; i++)
+    {
+        arr[i] = i+1;
+    }
+    
+
+    return 0;
+}
