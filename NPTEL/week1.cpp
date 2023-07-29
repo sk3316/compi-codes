27d94b00c2959d30fca1b01229ce65fa5f1d3231
sk3316 <sk3316@srmist.edu.in>
2023-07-30 01:20:50 +0530

    7th

--- NPTEL/week1.cpp
+++ NPTEL/week1.cpp
@@ -3,8 +3,14 @@
 #include<cstring>
 #include<string>
 #include<vector>
+#include<stack>
 #include<algorithm>
 using namespace std;
+
+void modify (int *arr){
+    rotate(arr, arr+3,arr+5);
+    rotate(arr, arr+2,arr+4);
+}
 int main()
 {   
     // q1.
@@ -38,14 +44,169 @@ int main()
     // }
 
 
-    string str1 = "welcome ";
-    string str2 = "students";
+    // string str1 = "welcome ";
+    // string str2 = "students";
     // str1+=str2;
-    // strcat(str1, str2);
+    // // strcat(str1, str2);
     // str1.append(str2);
-    str1.insert(str2);
-    cout<<str1;
+    // // str1.insert(str2);
+    // cout<<str1;
     
+
+    // char str[10] = "COMPUTER";
+    // stack<char> s1,s2;
+    // int i;
+    // for (int i = 0; i < strlen(str)/3; i++)
+    // {
+    //     s1.push(str[i]);
+    
+    // for (; i < strlen(str); i++)
+    
+    //     s2.push(str[i]);
+    // }
+    // while (!s1.empty()) 
+    // {
+    //     s2.push(s1.top()); s1.pop();
+    // }
+    // while (!s2.empty())
+    // {
+    //     cout<<s2.top(); s2.pop();
+    // }
+    
+    
+    int iarr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        *(iarr+i)= i*2;
+    }
+    modify(iarr);
+    for (int i = 0; i < 5; ++i)
+    {
+        cout<<*(iarr+i)<<" ";
+    }
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
